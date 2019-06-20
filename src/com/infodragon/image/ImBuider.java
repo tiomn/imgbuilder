f9224633e5f8401864b1908f82beeb8114515c37
tiomn <1833681033@qq.com>
2019-06-20 10:59:09 +0800

    增加背景透明化功能

--- src/com/infodragon/image/ImBuider.java
+++ src/com/infodragon/image/ImBuider.java
@@ -39,15 +39,15 @@ public class ImBuider {
 		// 行高------单行8MM
 		int rowheight = 56;
 		// 图片高度-------高度16MM
-		int imageHeight = 115;
+		int imageHeight = 118;
 		// 起始高度
 		int startHeight = 0;
 		// 起始宽度
 		int startWidth = 0;
 		//设置列宽
-		int colwidth = 84;
+		int colwidth = 112;
 		//设置自动图片宽度
-		int imageWidth = colwidth*(totalcol-1)+3;
+		int imageWidth = colwidth*(totalcol-1)+6;
 		BufferedImage image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
 		Graphics graphics = image.getGraphics();
 //		graphics.setColor(new Color(255, 255, 255,0));
@@ -60,6 +60,9 @@ public class ImBuider {
 			graphics.drawLine(startWidth, startHeight + (j) * rowheight, startWidth + colwidth * (totalcol-1)+j, startHeight + (j) * rowheight);
 			graphics.drawLine(startWidth, startHeight + (j) * rowheight+1, startWidth + colwidth * (totalcol-1)+j, startHeight + (j) * rowheight+1);
 			graphics.drawLine(startWidth, startHeight + (j) * rowheight+2, startWidth + colwidth * (totalcol-1)+j, startHeight + (j) * rowheight+2);
+			graphics.drawLine(startWidth, startHeight + (j) * rowheight+3, startWidth + colwidth * (totalcol-1)+j, startHeight + (j) * rowheight+3);
+			graphics.drawLine(startWidth, startHeight + (j) * rowheight+4, startWidth + colwidth * (totalcol-1)+j, startHeight + (j) * rowheight+4);
+			graphics.drawLine(startWidth, startHeight + (j) * rowheight+5, startWidth + colwidth * (totalcol-1)+j, startHeight + (j) * rowheight+5);
 
 		}
 		//画竖线
@@ -67,27 +70,29 @@ public class ImBuider {
 			graphics.setColor(Color.red);
 			graphics.drawLine(startWidth + k * colwidth, startHeight, startWidth + k * colwidth, startHeight + rowheight * (totalrow-1));
 			graphics.drawLine(startWidth+1 + k * colwidth, startHeight, startWidth + k * colwidth+1, startHeight + rowheight * (totalrow-1));
-			graphics.drawLine(startWidth+2 + k * colwidth, startHeight, startWidth + k * colwidth+2, startHeight + rowheight * (totalrow-1));
+			graphics.drawLine(startWidth+2 + k * colwidth, startHeight, startWidth + k * colwidth+2, startHeight + rowheight * (totalrow));
+			graphics.drawLine(startWidth+3 + k * colwidth, startHeight, startWidth + k * colwidth+3, startHeight + rowheight * (totalrow+1));
+			graphics.drawLine(startWidth+4 + k * colwidth, startHeight, startWidth + k * colwidth+4, startHeight + rowheight * (totalrow+2));
+			graphics.drawLine(startWidth+5 + k * colwidth, startHeight, startWidth + k * colwidth+5, startHeight + rowheight * (totalrow+3));
 		}
 		//设置字体
-		Font font = new Font("宋体", Font.PLAIN, fontTitileSize);
+		Font font = new Font("宋体", Font.BOLD, fontTitileSize);
 		graphics.setFont(font);
-		graphics.setColor(Color.BLACK);
+		graphics.setColor(Color.RED);
 		//写入内容
 		for (int n = 0; n < cellsValue.length; n++) {
 			for (int l = 0; l < cellsValue[n].length; l++) {
 				if (n == 0) {
-					font = new Font("宋体", Font.PLAIN, fontTitileSize);
+					font = new Font("宋体", Font.BOLD, fontTitileSize);
 					graphics.setFont(font);
 
 				} else {
-					font = new Font("宋体", Font.PLAIN, fontTitileSize);
+					font = new Font("宋体", Font.BOLD, fontTitileSize);
 					graphics.setFont(font);
 
 				}
 				graphics.drawString(cellsValue[n][l].toString(), startWidth + colwidth * l+6, startHeight + rowheight * (n+1)-15);
 			}
-
 		}
 		// 保存图片
 		createImage(image, path);
@@ -114,7 +119,9 @@ public class ImBuider {
 	}
 	
 	public static void main(String[] args) {
-//		String tableData1[][] = {{"为期该", "请勿且","湿哒哒"},{"创造性", "2019", "切尔在"}};
-//        ImBuider.myGraphicsGeneration(tableData1, "C:\\Users\\tm1612\\Desktop\\20190618.png");
+		String tableData1[][] = {{"TIOMN", "2019年","与思维来"},{"abcde", "永久", "になふちせな"}};
+        ImBuider.myGraphicsGeneration(tableData1, "C:\\Users\\tm1612\\Desktop\\20190618.png");
+        ImgManipulate imgManipulate = new ImgManipulate();
+        imgManipulate.transferAlpha2File("C:\\Users\\tm1612\\Desktop\\20190618.png", "C:\\Users\\tm1612\\Desktop\\20190618.png");
 	}
 }
